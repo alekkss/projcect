335ba573d5a691591ca0339a29ee48b2c4cf6c4e
Alex <dorina.el@yandex.ru>
2020-01-11 19:39:25 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,34 @@
+
+
+var Title = 33;
+let Title1 = 34;
+const Title2 = 35;
+
+console.log(4/2);
+console.log('string'/2);
+
+let persone = {
+    name: 'Igor',
+    age: 22
+};
+
+console.log(persone.name);
+
+let arr = ['gdgg','gdfggd','gdfv'];
+console.log(arr[0]);
+// alert('Hello World');
+// let answer = confirm('Как ты?');
+// console.log(answer);
+
+// let answer = prompt("Есть деньги", "Да есть!");
+// console.log(typeof(answer));
+// console.log('arr'+ " - object");
+
+let incr = 10,
+    decr = 10;
+incr++;
+decr--;
+console.log(incr);
+console.log(decr);
+
+
